cf6abb42108c207517d366ea604da8df13768b46
David Battefeld <post@davidbattefeld.de>
2026-02-14 20:44:45 +0100

    feat: add basic HTML structure

--- Sources/SkylarksAppConfig/configure.swift
+++ Sources/SkylarksAppConfig/configure.swift
@@ -4,10 +4,8 @@ import FluentSQLiteDriver
 import Leaf
 import Vapor
 
-// configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     app.databases.use(DatabaseConfigurationFactory.sqlite(.file("db.sqlite")), as: .sqlite)
 
