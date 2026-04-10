9b3f8712276f2b84b7dc7b1384d420f2b2c7ccaa
David Battefeld <post@davidbattefeld.de>
2026-04-10 09:59:56 +0200

    feat: add CORS config

--- Sources/SkylarksAppConfig/configure.swift
+++ Sources/SkylarksAppConfig/configure.swift
@@ -1,10 +1,27 @@
-import NIOSSL
 import Fluent
 import FluentSQLiteDriver
 import Leaf
+import NIOSSL
 import Vapor
 
 public func configure(_ app: Application) async throws {
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET],
+        allowedHeaders: [
+            .accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent,
+            .accessControlAllowOrigin,
+        ],
+        exposedHeaders: [
+            .accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent,
+            .accessControlAllowOrigin,
+        ],
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    
+    // cors middleware should come before default error middleware using `at: .beginning`
+    app.middleware.use(cors, at: .beginning)
+
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     app.databases.use(DatabaseConfigurationFactory.sqlite(.file("db.sqlite")), as: .sqlite)
