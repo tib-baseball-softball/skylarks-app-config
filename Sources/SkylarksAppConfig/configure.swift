404d750edc986583d74e9636502f068f1f472ad3
David Battefeld <post@davidbattefeld.de>
2026-04-12 18:06:05 +0200

    refactor/build: move sqlite db to subfolder for docker volume

--- Sources/SkylarksAppConfig/configure.swift
+++ Sources/SkylarksAppConfig/configure.swift
@@ -27,7 +27,7 @@ public func configure(_ app: Application) async throws {
 
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(DatabaseConfigurationFactory.sqlite(.file("db.sqlite")), as: .sqlite)
+    app.databases.use(DatabaseConfigurationFactory.sqlite(.file("vapor_data/db.sqlite")), as: .sqlite)
 
     app.migrations.add(CreateFirstSchema())
 
