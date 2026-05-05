88a73cfdcf216147edfde6eb4f6377ef99bbf133
David Battefeld <post@davidbattefeld.de>
2026-05-06 00:00:36 +0200

    feat: add healthcheck endpoint

--- Sources/SkylarksAppConfig/routes.swift
+++ Sources/SkylarksAppConfig/routes.swift
@@ -8,7 +8,15 @@ func routes(_ app: Application) throws {
     protected.get { req async throws in
         try await req.view.render("index", ["title": "Skylarks App Config Provider"])
     }
+    
+    app.get("api", "v1", "health") { req in
+        return HealthCheck(message: "API available")
+    }
 
     try app.register(collection: ConfigurationController())
     try app.register(collection: FeatureFlagController())
+}
+
+struct HealthCheck: Content {
+    let message: String
 }
